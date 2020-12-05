a5b93c6a7438bdcf7dcb17a57bb625faf0f84ee9
Markus Friemann <friemann26@gmail.com>
2020-12-05 16:10:41 +0100

    Mehr Dichtefunktionen, Histogramme. Anzahl Etagen Hist muss noch verbessert werden

--- Deskriptive Analyse.R
+++ Deskriptive Analyse.R
@@ -15,6 +15,14 @@ house_prices$sqmt_lot15 <- house_prices$sqft_lot15*0.092903
 house_prices$sqmt_above <- house_prices$sqft_above*0.092903
 house_prices$sqmt_basement <- house_prices$sqft_basement*0.092903
 
+# drop the variables with sqft
+house_prices_sqmt <- subset(house_prices, select = -c(house_prices$sqft_living,
+                                                      house_prices$sqft_living15,
+                                                      house_prices$sqft_lot,
+                                                      house_prices$sqft_lot15,
+                                                      house_prices$sqft_above,
+                                                      house_prices$sqft_basement))
+
 # attach the data to address the data in an easier way
 attach(house_prices)
 
@@ -33,15 +41,14 @@ min(price)
 t <- table(price) # Häufigkeitstabelle
 t[which.max(t)] # Modus
 summary(price)
-#boxplot(price, data=house_prices,varwidth=TRUE)
-#hist(price,xlim=c(0,1000000))
 
 # Eine Übersicht der Verteilung der Hauspreise über die Dichtefunktion gewinnen
 plot(density(price), main="Dichtefunktion: Hauspreise")
 abline(0,0,0,321950) # add 1st Quartile
 abline(0,0,0,645000) # add 3rd Quartile
 
-# Dichtefunktion aufzeigen
+# Dichtefunktion zoom in auf relevanten bereich
+plotprice <- function(){
 plot(density(price), main="Dichtefunktion: Hauspreise",xlim=c(0,2000000),xlab="Hauspreis",xaxt="n") # change x axis limit to exclude high outliers from the plot
 abline(0,0,0,321950) # add 1st Quartile
 abline(0,0,0,645000) # add 3rd Quartile
@@ -49,10 +56,46 @@ abline(0,0,0,645000) # add 3rd Quartile
 axis(1,at=c(0,321950,500000,645000,1000000,1500000),labels=c(0,321950,500000,645000,1000000,1500000)) # add custom x Axis Values
 text(380000,0.0000005,"- 1. Quartil")
 text(710000,0.0000005,"- 3. Quartil")
+}
+plotprice()
 
+
+# Observation:
 # Globales Maximum innerhalb der Dichtefunktion => mean und median wahrscheinlich auch innerhalb Interquartilsabstand
 # Dichtefunktion weist einen linksschiefen Verlauf aus. Das heißt median ist kleiner als das arithmetische Mittel
 
+detach(house_prices)
+attach(house_prices_sqmt)
+
+# Dichtefunktionen von price, sqmt_living15, sqmt_lot15, sqmt_basement
+par(mfrow=c(2,2))
+plotprice()
+plot(density(sqmt_living15),main="Wohnfläche in QM")
+plot(density(sqft_lot15),main="Grundstück in QM",xlim=c(0,50000))
+plot(density(sqmt_basement),main="Kellergroesse in QM",xlim=c(0,200))
+
+# wie ist ein Haus in King County aufgeteilt?
+# Histogramme mit Anzahl Schlafzimmer, Anzahl Badzimmer, Anzahl Etagen, Keller vorhanden?  
+par(mfrow=c(2,2))
+hist(bedrooms,main="Anzahl Schlafzimmer",xlim=c(0,8),xlab="Schlafzimmer",ylab="Anzahl Häuser",breaks=33,xaxt="n")
+axis(1,at=c(0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5),labels=c(0,1,2,3,4,5,6,7,8),pos=1) # add custom x Axis Values
+hist(bathrooms,main="Anzahl Badezimmer",xlim=c(0,6),xaxt="n",breaks=8,xlab="Badezimmer",ylab="Anzahl Häuser") # breaks at the max(bathrooms) to have 1 bar for each bathroom count
+axis(1,at=c(0.5,1.5,2.5,3.5,4.5,5.5,6.5),labels=c(0,1,2,3,4,5,6),pos=1) # add custom x Axis Values
+hist(floors,breaks=seq(1,3.5,0.25),xlab="Anzahl Etagen",xaxt="n",ylab="Anzahl Häuser")
+axis(1,at=c(1.125,1.375,1.875,2.375,3.875),labels=c(1,1.5,2,2.5,3),pos=1) # add custom x Axis Values
+
+?hist
+length(which(floors == 1))
+length(which(floors == 1.5))
+length(which(floors == 2))
+unique(floors)
+
+1910 + 10680
+max(floors)
+min(floors)
+
+# Wie viele Häuser haben 6 Schlafzimmer?
+length(which(bedrooms == 6))
 
 # looking at the 10 highest and lowest house prices
 sort_price <- house_prices[order(price),]
@@ -61,13 +104,13 @@ tail(sort_price$price,10)
 
 
 # Get an Overview of the dataset
-inspect(house_prices)
+inspect(house_prices_sqmt)
 # Obeservations: 
 # 1) No data is missing
 # 2) 5 Categorical variables, 1 date variable and 15 quantitative variables
 
 # View dataset
-View(house_prices)
+View(house_prices_sqmt)
 # Observations:
 # 1) 21.613 observations with 21 variables
 # 2) bathrooms variable can be split to 1/4 
